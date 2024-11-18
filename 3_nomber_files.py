820119fc51c18264a8e245598c53c8299a1ec252
Nikitin10a <nikitin.nikita1978@gmail.com>
2024-11-18 09:23:35 +0500

    Add files via upload

---
+++ 3_nomber_files.py
@@ -0,0 +1,8 @@
+from dataclasses import replace
+a = []
+with open('story.txt', 'r') as f:
+    for i in f:
+        replace(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+        a.append(i)
+with open('output.txt', 'w') as f1:
+    f1.write(a)
